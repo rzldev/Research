7460332a92988c36bf926da9ef55ff8b772925be
rzldev <amrizaldev00@gmail.com>
2019-08-04 17:28:06 +0700

    Import Module

---
+++ Python/9ImportModuleAndExploreLibrary.py
@@ -0,0 +1,5 @@
+
+## Import module
+from additional import my_modul
+
+city = ['New York', 'Chicago', 'Detroit', 'Gotham']
---
+++ Python/additional/my_modul.py
@@ -0,0 +1,10 @@
+print('Imported Modules...')
+
+test = 'Test String'
+
+def find_index(to_search, target) :
+    for i, value in enumerate(to_search) :
+        if value == target :
+            return 1
+
+    return -1
