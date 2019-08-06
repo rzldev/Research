13a1f0c8cf87e8064526e328d55c63164a53db06
rzldev <amrizaldev00@gmail.com>
2019-08-06 12:37:27 +0700

    Import Modules And Explore Libraries

--- Python/9ImportModuleAndExploreLibrary.py
+++ Python/9ImportModuleAndExploreLibrary.py
@@ -1,5 +1,76 @@
 
 ## Import module
-from additional import my_modul
+from additional import my_module
 
 city = ['New York', 'Chicago', 'Detroit', 'Gotham']
+
+index = my_module.find_index(city, 'Detroit')
+print(index)
+
+## Check a module's path
+import sys
+print(sys.path)
+
+## Using alias to call module
+from additional import my_module as mm
+
+new_index = mm.find_index(city, 'Gotham')
+print(new_index)
+
+## Import function inside module from specific path
+# Change python path to get the module
+sys.path.append('/home/rzl/Documents/Belajar/Python/additional')
+
+# import function
+from my_module import find_index
+
+index_function = find_index(city, 'New York')
+print(index_function)
+
+## Import variable inside module
+from my_module import find_index as fi, test as t
+
+new_fi = fi(city, 'Chicago')
+print(new_fi)
+print(t)
+
+## Import everything inside path or module
+from additional import *
+
+index2 = my_module.find_index(city, 'Gotham')
+test2 = my_module.test
+
+print(index2)
+print(test2)
+
+## Use random library
+import random
+
+random_city = random.choice(city)
+
+print(random_city)
+
+## Import math library
+import math
+
+rads = math.radians(90)
+
+print(rads)
+print(math.sin(rads))
+
+## Import datetime and calendar library
+import datetime
+import calendar
+
+today = datetime.date.today()
+print(today)
+print(calendar.isleap(2020))
+
+## Import OS library
+import os
+
+print(os.getcwd())
+print(os.__file__)
+
+## Import antigravity library
+import antigravity
--- Python/additional/my_module.py
+++ Python/additional/my_module.py
@@ -5,6 +5,6 @@ test = 'Test String'
 def find_index(to_search, target) :
     for i, value in enumerate(to_search) :
         if value == target :
-            return 1
+            return to_search.index(target)
 
     return -1
