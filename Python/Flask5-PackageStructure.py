f1550cd70375c10db49eb1e907386cb96aa3bf67
rzldev <amrizaldev00@gmail.com>
2019-10-27 15:37:26 +0700

    Flask

---
+++ Python/Flask5-PackageStructure.py
@@ -0,0 +1,58 @@
+from flask_sqlalchemy import SQLAlchemy
+from additionals.Flask import app, db
+from additionals.Flask.Models import User, Post
+
+db.drop_all()
+
+db.create_all()
+bruce = User(username='Bruce Wayne', email='Bruce@Richman.com', password='Batman')
+db.session.add(bruce)
+clark = User(username='Clark Kent', email='Kent@Superman.com', password='Superman')
+db.session.add(clark)
+db.session.commit()
+
+print(User.query.all())
+
+print()
+
+print(User.query.first())
+print(User.query.filter_by(username='Bruce Wayne').all())
+print(User.query.filter_by(username='Bruce Wayne').first())
+
+print()
+
+user = User.query.filter_by(username='Bruce Wayne').first()
+print(str(user.id))
+
+print()
+
+post1 = Post(title='The Arkham Knight', content='Arkham\'s Content', user_id=user.id)
+db.session.add(post1)
+post2 = Post(title='The Arham Asylum', content='Asylum\'s Content', user_id=user.id)
+db.session.add(post2)
+db.session.commit()
+
+print(user.posts)
+
+print()
+
+for post in user.posts :
+    print(post.title)
+
+print()
+
+post = Post.query.first()
+print(post)
+print(post.id)
+
+print()
+
+## Print post's author
+print(post.author)
+print()
+
+db.drop_all()
+
+## Automate debug and run flask
+if __name__ == '__main__' :
+    app.run(debug=True)
---
+++ Python/additionals/Flask/Routes.py
@@ -0,0 +1,58 @@
+from flask import render_template, flash, redirect, url_for
+from additionals.Flask import app
+from additionals.Flask.forms import RegistrationForm, LoginForm
+
+posts = [
+    {
+        'author' : 'Bruce Wayne',
+        'title' : 'Rise Of The Batman',
+        'content' : 'First Post Content',
+        'date_posted' : 'Oct 20, 2020'
+    },
+    {
+        'author' : 'Barry Allen',
+        'title' : 'The Flash Flashpoint Paradox',
+        'content' : 'Second Post Content',
+        'date_posted' : 'Oct 25, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home() :
+    return render_template('my-home.html', posts=posts, title='Home')
+
+@app.route("/about")
+def about () :
+    return render_template('my-about.html', title='About')
+
+@app.route("/index")
+def index () :
+    return render_template('index.html', posts=posts, title='Index')
+
+@app.route("/index=1")
+def index1() :
+    return render_template('index1.html', posts=posts, title="Page 1")
+
+@app.route("/index=2")
+def index2 () :
+    return render_template('index2.html', posts=posts, title='Page 2')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register () :
+    form = RegistrationForm()
+    if form.validate_on_submit() :
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login () :
+    form = LoginForm()
+    if form.validate_on_submit() :
+        if form.email.data == 'user@gmail.com' and form.password.data == 'password' :
+            flash(f'Welcome, {form.email.data}!', 'success')
+            return redirect(url_for('home'))
+        else :
+            flash(f'Login unsuccessful. Please check email and password!', 'danger')
+    return render_template('login.html', title='Login', form = form)
